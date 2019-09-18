f6141a2f1194072a3bb30dbf2cd30274370716e4
Benjamin Kyhn <benkyhn@hotmail.com>
2019-09-18 11:24:22 +0200

    Exercise 4.13 added

---
+++ src/Exercise_4_16.java
@@ -0,0 +1,41 @@
+public class Exercise_4_16{
+    public static void main(String args[]){
+        int number = (int)(Math.random()*29);
+
+            switch(number){
+                case 0: System.out.println("a"); break;
+                case 1: System.out.println("b"); break;
+                case 2: System.out.println("c"); break;
+                case 3: System.out.println("d"); break;
+                case 4: System.out.println("e"); break;
+                case 5: System.out.println("f"); break;
+                case 6: System.out.println("g"); break;
+                case 7: System.out.println("h"); break;
+                case 8: System.out.println("i"); break;
+                case 9: System.out.println("j"); break;
+                case 10: System.out.println("k"); break;
+                case 11: System.out.println("l"); break;
+                case 12: System.out.println("m"); break;
+                case 13: System.out.println("n"); break;
+                case 14: System.out.println("o"); break;
+                case 15: System.out.println("p"); break;
+                case 16: System.out.println("q"); break;
+                case 17: System.out.println("r"); break;
+                case 18: System.out.println("s"); break;
+                case 19: System.out.println("t"); break;
+                case 20: System.out.println("u"); break;
+                case 21: System.out.println("v"); break;
+                case 22: System.out.println("w"); break;
+                case 23: System.out.println("x"); break;
+                case 24: System.out.println("y"); break;
+                case 25: System.out.println("z"); break;
+                case 26: System.out.println("æ"); break;
+                case 27: System.out.println("ø"); break;
+                case 28: System.out.println("å"); break;
+
+
+
+            }
+
+    }
+}
